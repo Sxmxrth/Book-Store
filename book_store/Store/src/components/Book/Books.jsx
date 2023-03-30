88c9d24a1e3676dcd1071d0740cea80f119ec417
Samarth Gupta <samarthgupta1202@gmail.com>
2023-03-30 11:05:05 +0530

    Card components rendered

--- book_store/Store/src/components/Book/Books.jsx
+++ book_store/Store/src/components/Book/Books.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
+import Book from './Book'
+import "./Book.css"
 const URL = "http://localhost:5000/books"
 
 const fetchHandler = async () => {
@@ -9,14 +11,20 @@ const fetchHandler = async () => {
 const Books = () => {
     const [books, setBooks] = useState();
     useEffect(() => {                           //side effects
-        fetchHandler().then((data) => setBooks(data))
+        fetchHandler().then((data) => setBooks(data.books))  //this .books is from the array
     },[])
     console.log(books);
   return (
     <div>
-      Books
+      <ul>{books && books.map((book, i) => {
+        return(<li key = {i}>
+          <Book book = {book} />
+        </li>)
+      })}
+      </ul>
     </div>
-  )
+  ) 
 }
 
 export default Books
+
